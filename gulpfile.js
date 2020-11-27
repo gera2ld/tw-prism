894f03d5d2352a0a7f1934775279c693ab5f751a
Gerald <gera2ld@live.com>
2020-11-27 15:28:41 +0800

    fix: exclude some languages that I dont know

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const order = require('gulp-order');
 const replace = require('gulp-replace');
 const terser = require('gulp-terser-js');
 const del = require('del');
@@ -8,11 +7,71 @@ const components = require('prismjs/components');
 const pkg = require('./package.json');
 
 const DIST = 'dist';
-const languages = Object.keys(components.languages)
-  .filter(key => key !== 'meta');
-const keys = ['core'];
-languages.forEach(addLanguage);
-const orderKeys = keys.map(key => `**/prism-${key}.js`);
+// const languages = Object.keys(components.languages).filter(key => key !== 'meta');
+const keys = [];
+[
+  'markup',
+  'css',
+  'clike',
+  'javascript',
+  'apacheconf',
+  'bash',
+  'batch',
+  'brainfuck',
+  'c',
+  'csharp',
+  'cpp',
+  'cmake',
+  'coffeescript',
+  'css-extras',
+  'diff',
+  'dns-zone-file',
+  'docker',
+  'editorconfig',
+  'ejs',
+  'git',
+  'glsl',
+  'go',
+  'graphql',
+  'handlebars',
+  'haxe',
+  'http',
+  'icon',
+  'ignore',
+  'ini',
+  'jq',
+  'jsdoc',
+  'js-extras',
+  'json',
+  'json5',
+  'jsonp',
+  'jsstacktrace',
+  'js-templates',
+  'kotlin',
+  'latex',
+  'less',
+  'lua',
+  'makefile',
+  'markdown',
+  'markup-templating',
+  'matlab',
+  'mongodb',
+  'nginx',
+  'ocaml',
+  'powershell',
+  'pug',
+  'python',
+  'jsx',
+  'tsx',
+  'regex',
+  'rust',
+  'sql',
+  'toml',
+  'typescript',
+  'vim',
+  'wasm',
+  'yaml',
+].forEach(addLanguage);
 
 function addLanguage(key) {
   if (keys.includes(key)) return;
@@ -28,10 +87,9 @@ function clean() {
 
 function build() {
   return gulp.src([
-    'node_modules/prismjs/components/prism-*.js',
-    '!node_modules/prismjs/components/prism-*.min.js',
+    'node_modules/prismjs/components/prism-core.js',
+    ...keys.map(key => `node_modules/prismjs/components/prism-${key}.min.js`),
   ])
-    .pipe(order(orderKeys))
     .pipe(concat('prism.js', { newLine: ';' }))
     .pipe(terser())
     .pipe(gulp.dest(`${DIST}/files`));
